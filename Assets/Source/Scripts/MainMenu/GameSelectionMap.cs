dca0e31745e38f55d4a1c1d65161b3f3dd8556f4
JefOpinionStratton <jeffrey.opinon@strattonstudiogames.com>
2024-12-03 15:28:12 +0800

    New UI for matchmaking

---
+++ Assets/Source/Scripts/MainMenu/GameSelectionMap.cs
@@ -0,0 +1,81 @@
+﻿using MultiFPS;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StrattonStudioGames.PrisMulti
+{
+    public class GameSelectionMap : MonoBehaviour
+    {
+        [Header("Scene references")]
+        [SerializeField] private Toggle toggle;
+        [SerializeField] private ToggleGroup toggleGroup;
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private GameSelectionPanel gameSelectionPanel;
+        
+        [Header("Asset references")]
+        [SerializeField] private MapRepresenter mapData;
+
+        #region Unity hooks
+
+        private void OnEnable()
+        {
+            toggle.onValueChanged.AddListener(OnToggle);
+        }
+
+        private void OnDisable()
+        {
+            toggle.onValueChanged.RemoveListener(OnToggle);
+        }
+
+        private void Reset()
+        {
+            toggle = GetComponent<Toggle>();
+            toggleGroup = GetComponent<ToggleGroup>();
+            canvasGroup = GetComponent<CanvasGroup>();
+            rectTransform = GetComponent<RectTransform>();
+        }
+
+        #endregion
+
+        [ContextMenu(nameof(Show))]
+        private void Show()
+        {
+            OnToggle(true);
+        }
+
+        [ContextMenu(nameof(Hide))]
+        private void Hide()
+        {
+            OnToggle(false);
+        }
+
+        private void OnToggle(bool isOn)
+        {
+            if (isOn)
+            {
+                var index = transform.GetSiblingIndex();
+                gameSelectionPanel.SelectMap(mapData);
+                
+                rectTransform.sizeDelta += new Vector2(0, 60);
+                canvasGroup.alpha = 1;
+                canvasGroup.interactable = true;
+                canvasGroup.blocksRaycasts = true;
+            }
+            else
+            {
+                gameSelectionPanel.SelectMap(null);
+                
+                rectTransform.sizeDelta -= new Vector2(0, 60);
+                canvasGroup.alpha = 0;
+                canvasGroup.interactable = false;
+                canvasGroup.blocksRaycasts = false;
+                
+                foreach (var entry in toggleGroup.ActiveToggles())
+                {
+                    entry.isOn = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Source/Scripts/MainMenu/GameSelectionPanel.cs
@@ -0,0 +1,97 @@
+﻿using DNServerList;
+using MultiFPS;
+using MultiFPS.Gameplay.Gamemodes;
+using MultiFPS.ServerList;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StrattonStudioGames.PrisMulti
+{
+    public class GameSelectionPanel : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Image mapPreview;
+        [SerializeField] private Button findMatchButton;
+        [SerializeField] private ToggleGroup toggleGroup;
+        [SerializeField] private GameSettingsSO gameSettings;
+
+        private MapRepresenter selectedMap;
+        private Gamemodes? selectedGamemode;
+
+        #region MonoBehaviour events
+
+        private void Start()
+        {
+            ValidateRequest();
+            mapPreview.color = Color.clear;
+        }
+
+        private void OnEnable()
+        {
+            findMatchButton.onClick.AddListener(FindMatch);
+        }
+
+        private void OnDisable()
+        {
+            findMatchButton.onClick.RemoveListener(FindMatch);
+        }
+
+        #endregion
+
+        public void SelectMap(MapRepresenter mapRepresenter)
+        {
+            selectedMap = mapRepresenter;
+            
+            if (mapRepresenter)
+            {
+                mapPreview.sprite = mapRepresenter.Icon;
+                mapPreview.color = Color.white;
+            }
+            else
+            {
+                mapPreview.sprite = null;
+                mapPreview.color = Color.clear;
+            }
+
+            ValidateRequest();
+        }
+
+        public void SelectMode(int? index)
+        {
+            if (index.HasValue)
+            {
+                selectedGamemode = selectedMap.AvailableGamemodes[index.Value];
+            }
+            else
+            {
+                selectedGamemode = null;
+            }
+            
+            ValidateRequest();
+        }
+
+        public void FindMatch()
+        {
+            findMatchButton.interactable = false;
+
+            var networkManager = DNNetworkManager.Instance;
+            
+            networkManager.onlineScene =  selectedMap.Scene;
+            RoomSetup.Properties.P_FillEmptySlotsWithBots = true;
+            RoomSetup.Properties.P_Gamemode = selectedGamemode.Value;
+            RoomSetup.Properties.P_RespawnCooldown = 6f;
+            RoomSetup.Properties.P_GameDuration = gameSettings.GameDurations[^1];
+
+            var maxPlayers = selectedMap.MaxPlayersPresets[^1];
+            RoomSetup.Properties.P_MaxPlayers = maxPlayers; // for gamemode
+            networkManager.maxConnections = maxPlayers; // for handling connections
+            networkManager.StartHost();
+        }
+
+        private void ValidateRequest()
+        {
+            var isValid = selectedMap != null && selectedGamemode != null;
+            findMatchButton.interactable = isValid;
+        }
+    }
+}
