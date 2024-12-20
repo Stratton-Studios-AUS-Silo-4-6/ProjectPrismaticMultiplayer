38c5a63882f95918bc0a3d2f8e906aca8becdbf2
JefOpinionStratton <jeffrey.opinon@strattonstudiogames.com>
2024-12-20 14:59:01 +0800

    Added Scenes menu item
    - Utility for moving between frequently-used scenes

---
+++ Assets/Source/Scripts/Editor/SceneUtility.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace StrattonStudioGames.PrisMulti.Editor
+{
+    public static class SceneUtility
+    {
+        [MenuItem("Scenes/MainMenu/Matchmaking")]
+        private static void ToLogin()
+        {
+            EditorSceneManager.OpenScene("Assets/Source/MainMenu/Matchmaking.unity");
+        }
+
+        [MenuItem("Scenes/Gameplay/Range")]
+        private static void ToRange()
+        {
+            EditorSceneManager.OpenScene("Assets/MultiFPS/Levels/map_range.unity");
+        }
+
+        [MenuItem("Scenes/Gameplay/CircleArena")]
+        private static void ToCircleArena()
+        {
+            EditorSceneManager.OpenScene("Assets/MultiFPS/Levels/map_circleArena.unity");
+        }
+
+        [MenuItem("Scenes/Gameplay/Construction")]
+        private static void ToConstruction()
+        {
+            EditorSceneManager.OpenScene("Assets/MultiFPS/Levels/map_construction.unity");
+        }
+
+        [MenuItem("Scenes/Gameplay/BombBuilding")]
+        private static void ToBombBuilding()
+        {
+            EditorSceneManager.OpenScene("Assets/MultiFPS/Levels/map_bombBuilding.unity");
+        }
+
+        [MenuItem("Scenes/Gameplay/Arena")]
+        private static void ToArena()
+        {
+            EditorSceneManager.OpenScene("Assets/MultiFPS/Levels/map_arena.unity");
+        }
+    }
+}
