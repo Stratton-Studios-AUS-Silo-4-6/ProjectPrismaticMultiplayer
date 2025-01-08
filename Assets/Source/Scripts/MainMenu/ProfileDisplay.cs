e81b262311204b5fdc1983778f4919504cdcbe66
JefOpinionStratton <jeffrey.opinon@strattonstudiogames.com>
2025-01-08 10:50:10 +0800

    Cleanup
    - Removed TestBeam script
    - Created CurrencyDisplay and ProfileDisplay script
    - Created LoadingIndicator prefab
    - Updated Matchmaking scene

---
+++ Assets/Source/Scripts/MainMenu/ProfileDisplay.cs
@@ -0,0 +1,50 @@
+﻿using Beamable;
+using Beamable.Avatars;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StrattonStudioGames.PrisMulti
+{
+    /// <summary>
+    /// Component that integrates Beamable Account system to display currencies in UI objects.
+    /// </summary>
+    public class ProfileDisplay : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI alias;
+        [SerializeField] private Image avatar;
+
+        #region Unity hooks
+
+        private void Start()
+        {
+            Clear();
+            Refresh();
+        }
+
+        #endregion
+
+        private void Clear()
+        {
+            alias.text = string.Empty;
+            avatar.color = Color.clear;
+        }
+
+        private async void Refresh()
+        {
+            var context = BeamContext.Default;
+
+            await context.OnReady;
+            await context.Accounts.Refresh();
+
+            var account = context.Accounts.Current;
+            
+            alias.text = account.Alias;
+            
+            var avatarConfig = context.ServiceProvider.GetService<AvatarConfiguration>();
+            avatar.color = Color.white;
+            avatar.sprite = avatarConfig.Avatars[0].Sprite;
+            Debug.Log($"avatar id: {account.Avatar}");
+        }
+    }
+}
