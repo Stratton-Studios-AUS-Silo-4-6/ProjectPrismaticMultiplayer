df8c6757e1e2ffcfdd38125b1b521bdb368adb8a
JefOpinionStratton <jeffrey.opinon@strattonstudiogames.com>
2025-01-29 13:41:38 +0800

    Initial setup for gun cosmetics using skins
    - Created scriptable objects and singletons for cosmetics and cosmetics database to be used at runtime
    - Added CosmeticLoader component to prototype cosmetics behavior

---
+++ Assets/Source/Scripts/Inventory/CosmeticLoader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace StrattonStudioGames.PrisMulti
+{
+    public class CosmeticLoader : MonoBehaviour
+    {
+        /// <summary>
+        /// Id of the item. Used to check what is the equipped cosmetic in the loadout for this item.
+        /// </summary>
+        [SerializeField] private string itemId;
+        
+        /// <summary>
+        /// Placeholder field. Direct reference to the cosmetic id which loads the cosmetic for the item.
+        /// </summary>
+        [SerializeField] private string cosmeticId;
+        
+        [SerializeField] private SkinnedMeshRenderer skinnedMeshRenderer;
+
+        private void Start()
+        {
+            if (CosmeticDatabase.Instance.TryFind<GunCosmetic>(cosmeticId, out var gunCosmetic))
+            {
+                gunCosmetic.Apply(skinnedMeshRenderer);
+            }
+            else
+            {
+                Debug.LogError($"Could not find cosmetic of id [{cosmeticId}].");
+            }
+        }
+    }
+}
